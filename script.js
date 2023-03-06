29fc3da126dfc91655431ee6be489b45166a3747
Jasmine Yip <yippie.web@gmail.com>
2023-03-06 22:26:55 +1300

    Add testimonial slider

--- script.js
+++ script.js
@@ -37,17 +37,29 @@ document.querySelectorAll('.accordion__button').forEach(button => {
 
 
 // Testimonial slider
-const slidesContainer = document.getElementById("slides-container");
-const slide = document.querySelector(".slide");
-const prevButton = document.getElementById("slide-arrow-prev");
-const nextButton = document.getElementById("slide-arrow-next");
+var slideIndex = 1;
+showSlides(slideIndex);
 
-nextButton.addEventListener("click", (event) => {
-    const slideWidth = slide.clientWidth;
-      slidesContainer.scrollLeft += slideWidth;
-});
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-prevButton.addEventListener("click", () => {
-const slideWidth = slide.clientWidth;
-slidesContainer.scrollLeft -= slideWidth;
-});
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
