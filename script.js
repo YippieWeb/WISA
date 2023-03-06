8af5cbe8a03ae46b5c836b0373a017e0d54957df
Jasmine Yip <yippie.web@gmail.com>
2023-03-06 20:46:36 +1300

    Make responsive

--- script.js
+++ script.js
@@ -20,3 +20,34 @@ ako.src = 'assets/audio/ako.mp3';
     });
 });
 
+// Accordion
+document.querySelectorAll('.accordion__button').forEach(button => {
+    button.addEventListener('click', () => { 
+        // const accordionContent = button.nextElementSibling;
+
+        button.classList.toggle('accordion__button--active');
+
+        // if (button.classList.contains('accordion__button--active')){
+        //    accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+        // } else {
+        //    accordionContent.style.maxHeight = 0;
+        //}
+    });
+});
+
+
+// Testimonial slider
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+nextButton.addEventListener("click", (event) => {
+    const slideWidth = slide.clientWidth;
+      slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+const slideWidth = slide.clientWidth;
+slidesContainer.scrollLeft -= slideWidth;
+});
